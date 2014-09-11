9d130e0eff031759b6f0b372d680b159fcb440be
Geoff McLane <ubuntu@geffair.info>
2014-09-11 14:35:37 +0200

    Update of index page

--- gi.bat
+++ gi.bat
@@ -1,5 +1,27 @@
 @setlocal
 @echo Generate a new 'index' table...
-call genindex . -d description.csv -b -l -s
+@set TMPOPTS=-v -b -s
 
+@REM add ignore.txt if it exists
+@if EXIST ignore.txt (
+@set TMPOPTS=%TMPOPTS% -X ignore.txt
+)
 
+@REM add description.csv if it exists
+@if EXIST description.csv (
+@set TMPOPTS=%TMPOPTS% -d description.csv
+)
+
+@set TMPOPTS=%TMPOPTS% -x temp*
+
+:RPT
+@if "%~1x" == "x" goto GOTCMD
+@set TMPOPTS=%TMPOPTS% %1
+@shift
+@goto RPT
+
+:GOTCMD
+
+call genindex %TMPOPTS% .
+
+@REM eof
